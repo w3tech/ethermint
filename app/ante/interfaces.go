4cbdb83400af336a19112726ec7c6e3ac25e111c
yihuang <huang@crypto.com>
2024-04-03 10:17:46 +0800

    Problem: transient store usage not compatible with parallel tx execution (#450)
    * Problem: transient store usage not compatible with parallel tx execution
    
    Currently we use shared transient store keys to accumulate some states,
    which cause issues when developing parallel tx execution
    
    Solution:
    - remove some transient stores.
    - the others are used in a per-tx fasion.
    
    Update CHANGELOG.md
    
    Signed-off-by: yihuang <huang@crypto.com>
    
    cleanup
    
    fix test
    
    * fix
    
    * cleanup

--- app/ante/interfaces.go
+++ app/ante/interfaces.go
@@ -46,8 +46,6 @@ type EVMKeeper interface {
 
 	NewEVM(ctx sdk.Context, msg core.Message, cfg *statedb.EVMConfig, tracer vm.EVMLogger, stateDB vm.StateDB) evm.EVM
 	DeductTxCostsFromUserBalance(ctx sdk.Context, fees sdk.Coins, from common.Address) error
-	GetBalance(ctx sdk.Context, addr sdk.AccAddress, denom string) *big.Int
-	GetParams(ctx sdk.Context) evmtypes.Params
 }
 
 type protoTxProvider interface {
