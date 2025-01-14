3954aade41efc320275a6905a25d82c04015a853
Kyuhyeon Choi <bluejet1104@gmail.com>
2025-01-14 21:59:28 +0900

    fix: update init.sh to fix consensus failure issue

--- init.sh
+++ init.sh
@@ -34,7 +34,7 @@ cat $HOME/.ethermintd/config/genesis.json | jq '.app_state["gov"]["deposit_param
 cat $HOME/.ethermintd/config/genesis.json | jq '.app_state["mint"]["params"]["mint_denom"]="aphoton"' > $HOME/.ethermintd/config/tmp_genesis.json && mv $HOME/.ethermintd/config/tmp_genesis.json $HOME/.ethermintd/config/genesis.json
 
 # Set gas limit in genesis
-cat $HOME/.ethermintd/config/genesis.json | jq '.consensus_params["block"]["max_gas"]="20000000"' > $HOME/.ethermintd/config/tmp_genesis.json && mv $HOME/.ethermintd/config/tmp_genesis.json $HOME/.ethermintd/config/genesis.json
+cat $HOME/.ethermintd/config/genesis.json | jq '.consensus["params"]["block"]["max_gas"]="20000000"' > $HOME/.ethermintd/config/tmp_genesis.json && mv $HOME/.ethermintd/config/tmp_genesis.json $HOME/.ethermintd/config/genesis.json
 
 # Allocate genesis accounts (cosmos formatted addresses)
 ethermintd genesis add-genesis-account $KEY 100000000000000000000000000aphoton --keyring-backend $KEYRING
