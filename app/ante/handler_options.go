6a4b44f69e7938d057da049a9de6a10f60cddc05
tom <tomasguerraalda@hotmail.com>
2025-02-20 18:02:59 -0300

    fix linter

--- app/ante/handler_options.go
+++ app/ante/handler_options.go
@@ -35,18 +35,18 @@ import (
 // HandlerOptions extend the SDK's AnteHandler options by requiring the IBC
 // channel keeper, EVM Keeper and Fee Market Keeper.
 type HandlerOptions struct {
-	AccountKeeper           evmtypes.AccountKeeper
-	BankKeeper              evmtypes.BankKeeper
-	IBCKeeper               *ibckeeper.Keeper
-	FeeMarketKeeper         FeeMarketKeeper
-	EvmKeeper               EVMKeeper
-	FeegrantKeeper          ante.FeegrantKeeper
-	SignModeHandler         *txsigning.HandlerMap
-	SigGasConsumer          func(meter storetypes.GasMeter, sig signing.SignatureV2, params authtypes.Params) error
-	MaxTxGasWanted          uint64
-	ExtensionOptionChecker  ante.ExtensionOptionChecker
-	TxFeeChecker            ante.TxFeeChecker
-	DisabledAuthzMsgs       []string
+	AccountKeeper          evmtypes.AccountKeeper
+	BankKeeper             evmtypes.BankKeeper
+	IBCKeeper              *ibckeeper.Keeper
+	FeeMarketKeeper        FeeMarketKeeper
+	EvmKeeper              EVMKeeper
+	FeegrantKeeper         ante.FeegrantKeeper
+	SignModeHandler        *txsigning.HandlerMap
+	SigGasConsumer         func(meter storetypes.GasMeter, sig signing.SignatureV2, params authtypes.Params) error
+	MaxTxGasWanted         uint64
+	ExtensionOptionChecker ante.ExtensionOptionChecker
+	TxFeeChecker           ante.TxFeeChecker
+	DisabledAuthzMsgs      []string
 }
 
 func (options HandlerOptions) validate() error {
